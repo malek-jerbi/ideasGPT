03c4e382a283621f8c4fc971ed306c4940bd66c8
Samuel Chuang <samuelchuang98@gmail.com>
2023-04-04 15:07:01 -0400

    Small changes to user profile

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,6 +6,15 @@ import Navbar from 'react-bootstrap/Navbar'
 import LoginButton from './LoginButton'
 import LogoutButton from './LogoutButton'
 
+// q: Where can I find the CSS for the Navbar component?
+// a: The CSS for the Navbar component is in the following file:
+// C:\Users\james\Documents\GitHub\IdeasGPT\frontend\node_modules\react-bootstrap\src\Navbar.js
+
+// q: Where can I do custom CSS on the Navbar component?
+// a: I can do custom CSS on the Navbar component by adding a className prop to the Navbar component
+
+// q: I want to move the
+
 const Header = () => {
   return (
     <header>
@@ -16,7 +25,7 @@ const Header = () => {
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='mr-auto'>
+            <Nav className='mr-auto nav'>
               <LinkContainer to='/'>
                 <Nav.Link>Home</Nav.Link>
               </LinkContainer>
--- frontend/src/screens/UserProfileScreen/UserProfileScreen.js
+++ frontend/src/screens/UserProfileScreen/UserProfileScreen.js
@@ -1,5 +1,5 @@
 import { React, useEffect, useContext } from 'react';
-import { Box }from '@mui/material';
+import { Box } from '@mui/material';
 import AuthContext from "../../components/AuthContext.js";
 import {styled } from '@mui/system'
 import UsersIdeaTable from '../../components/UsersIdeaTable/UsersIdeaTable.js';
@@ -8,13 +8,20 @@ import userApi from '../../api/UserApi.js';
 
 
 
+/* q: Why am I getting the following error when I try to use the styled function from @mui/system?
+ERROR in ./src/screens/UserProfileScreen/UserProfileScreen.js 13:18-24
+export 'styled' (imported as 'styled') was not found in '@mui/system' (module has no exports)*/
 
+// q: answer the question above
+// a: I had to install the @mui/styled-engine-sc package to get the styled function to work
 
 const StyledBox = styled(Box,{})({
   color:"6B068",
-  backgroundColor: "silver",
+  backgroundColor: "#F5F5F5",
+  boxShadow: "0px 0px 10px 0px rgba(0,0,0,0.75)",
   margin: "auto",
-  borderRadius: 2,
+  marginTop: 25,
+  borderRadius: 50,
   height: 1000,
   width: 1000,
   display: "flex",
@@ -61,6 +68,40 @@ const UserProfileScreen = () => {
 
 
   return (
+
+    // Add code to display "Hello" followed by user's name
+    // q: where can I find the user's name?
+    // a: it's in the dbUser object
+    // q: how do I access the dbUser object?
+    // a: use the useContext hook
+    // q: how do I use the useContext hook?
+    // a: import the AuthContext.js file and use the useContext hook
+    // q: where can I find the AuthContext.js file?
+    // a: it's in the components folder
+    // q: how do I import the AuthContext.js file?
+    // a: import AuthContext from "../../components/AuthContext.js";
+    // q: how do I use the useContext hook?
+    // a: const { dbUser, setdbUser } = useContext(AuthContext);
+    // q: how do I access the dbUser object?
+    // a: dbUser
+    // q: how do I access the user's name?
+    // a: dbUser.name
+    // q: how do I display the user's name?
+    // a: {dbUser.name}
+    // q: how do I display "Hello" followed by the user's name?
+    // a: Hello {dbUser.name}
+    // q: how do I display "Hello" followed by the user's name in a <h1> tag?
+    // a: <h1>Hello {dbUser.name}</h1>
+    // q: how do I display "Hello" followed by the user's name in a <h1> tag in the center of the screen?
+    // a: <h1 style={{textAlign: "center"}}>Hello {dbUser.name}</h1>
+    // q: how do I display "Hello" followed by the user's name in a <h1> tag in the center of the screen with a background color of #F5F5F5?
+    // a: <h1 style={{textAlign: "center", backgroundColor: "#F5F5F5"}}>Hello {dbUser.name}</h1>
+    // q: how do I display "Hello" followed by the user's name in a <h1> tag in the center of the screen with a background color of #F5F5F5 and a border radius of 50?
+    // a: <h1 style={{textAlign: "center", backgroundColor: "#F5F5F5", borderRadius: 50}}>Hello {dbUser.name}</h1>
+    // q: how do I display "Hello" followed by the user's name in a <h1> tag in the center of the screen with a background color of #F5F5F5 and a border radius of 50 and a box shadow?
+    // a: <h1 style={{textAlign: "center", backgroundColor: "#F5F5F5", borderRadius: 50, boxShadow: "0px 0px 10px 0px rgba(0,0,0,0.75)"}}>Hello {dbUser.name}</h1>
+
+    
 
     <StyledBox>
 
