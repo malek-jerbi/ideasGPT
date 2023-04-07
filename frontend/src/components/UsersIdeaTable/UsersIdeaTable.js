3a706edd3e7c89647ddfc9d03b5d9c3de6f4e855
Samuel Chuang <72810981+SamuelChuang98@users.noreply.github.com>
2023-04-06 20:06:57 -0400

    small changes to usersideatable

--- frontend/src/components/UsersIdeaTable/UsersIdeaTable.js
+++ frontend/src/components/UsersIdeaTable/UsersIdeaTable.js
@@ -1,42 +1,46 @@
-import { TableCell, TableContainer, TableHead, TableRow , Table, TableBody, Paper} from "@mui/material";
-import React from "react";
-
+import { TableCell, TableContainer, TableHead, TableRow , Table, TableBody, Paper, Button } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 const UsersIdeaTable = ({likedIdeas}) => {
-    console.log(likedIdeas);
+    const [ideasData, setIdeasData] = useState([]);
 
+    useEffect(() => {
+        axios.get('/ideas')
+            .then(res => {
+                const ideas = res.data;
+                const likedIdeasData = ideas.filter(idea => likedIdeas.includes(idea._id));
+                setIdeasData(likedIdeasData);
+            })
+            .catch(err => console.log(err));
+    }, [likedIdeas]);
+
+    console.log(ideasData);
 
     return (
-
         <TableContainer overflow-y="scrollS" component={Paper} sx={{maxHeight: '200px'}}>
-        <Table options={{maxBodyHeight:100}}>
-            <TableHead>
-                <TableRow>
-                    <TableCell>Description</TableCell>
-                    <TableCell>Claim</TableCell>
-                </TableRow>
-            </TableHead>
-            <TableBody>
-            {likedIdeas.map((ideas) => (
-                
-                <TableRow key={ideas._id}>
-                    <TableCell>{ideas.description}</TableCell>
-                </TableRow>
-
-            ))}
-            </TableBody>
-            <TableBody>
-
-            </TableBody>
-
-        </Table>
-    </TableContainer>
+            <Table options={{maxBodyHeight:100}}>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Description</TableCell>
+                        <TableCell>Claim</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {ideasData.map((idea) => (
+                        <TableRow key={idea._id}>
+                            <TableCell>{idea.text}</TableCell>
+                            <TableCell>
+                                <Button variant="contained" color="primary">
+                                    Claim
+                                </Button>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
     )
-
-    
-
-
-
 }
 
 export default UsersIdeaTable;
