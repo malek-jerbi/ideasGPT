836f4d8ee1bf6cfe481db6b629634f51d908540a
jainil jaha <jainiljaha@gmail.com>
2023-04-09 19:29:58 -0400

    added the claim feature + claimed table

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,90 +1,93 @@
 import User from '../models/userModel.js'
 import Idea from '../models/ideaModel.js'
 
-// save new user to database
+const getUserByEmail = async (email) => {
+  return await User.findOne({ email });
+}
+
+const getUserByAuth0Id = async (auth0Id) => {
+  return await User.findOne({ auth0Id });
+}
+
+const createNewUser = async (email, name, auth0Id) => {
+  const user = new User({ email, name, auth0Id });
+  return await user.save();
+}
+
+const updateUserAuth0Id = async (user, auth0Id) => {
+  user.auth0Id = auth0Id;
+  return await user.save();
+}
+
 export const createUser = async (req, res) => {
   try {
     console.log('DEBUG: Inside CreateUser()')
 
-    const { email, name } = req.body
-    const auth0Id = req.auth.payload.sub
+    const { email, name } = req.body;
+    const auth0Id = req.auth.payload.sub;
 
-    let user = await User.findOne({ auth0Id })
+    let user = await getUserByAuth0Id(auth0Id);
 
     if (user) {
-      console.log('DEBUG: Found Existing User')
+      console.log('DEBUG: Found Existing User');
       res.status(200).json({
         success: true,
-        error: 'User is already saved so will be retruning it',
+        error: 'User is already saved so will be returning it',
         data: user,
-      })
+      });
     } else {
-      user = await User.findOne({ email })
+      user = await getUserByEmail(email);
 
       if (user) {
-        console.log('DEBUG: Found User with Same Email')
-        user.auth0Id = auth0Id
-        await user.save()
+        console.log('DEBUG: Found User with Same Email');
+        user = await updateUserAuth0Id(user, auth0Id);
       } else {
-        console.log('DEBUG: Creating New User')
-        user = new User({ email: email, name: name, auth0Id: auth0Id })
-        await user.save()
+        console.log('DEBUG: Creating New User');
+        user = await createNewUser(email, name, auth0Id);
       }
 
       res.status(201).json({
         success: true,
-        message: 'User Has been saved ',
+        message: 'User Has been saved',
         data: user,
-      })
+      });
     }
   } catch (err) {
-    console.error(err)
-    res.status(500).send()
+    console.error(err);
+    res.status(500).send();
   }
 }
 
-// gets user by ID
-
 export const getUserByID = async (req, res) => {
   try {
-    console.log('DEBUG: InsideGetUserByID')
+    console.log('DEBUG: InsideGetUserByID');
 
-    const userID = req.params.id
+    const userID = req.params.id;
 
-    const user = await User.findById(userID)
+    const user = await User.findById(userID);
 
-    // No user found
     if (!user) {
       return res
         .status(404)
-        .json({ status: 'fail', message: 'No user found with that ID' })
+        .json({ status: 'fail', message: 'No user found with that ID' });
     }
-    res.status(200).json({ status: 'success', data: user })
+    res.status(200).json({ status: 'success', data: user });
 
-    res.status(200).json({
-      success: true,
-      message: 'User Returned',
-      data: user,
-    })
   } catch (err) {
-    // Invalid id
-    res.status(400).json({ status: 'fail', message: err })
+    res.status(400).json({ status: 'fail', message: err });
   }
 }
 
 export const swipe = async (req, res) => {
   try {
-    // Get the required information from the request body
-    const { ideaId, action } = req.body
-    const userId = req.auth.payload.sub
+    const { ideaId, action } = req.body;
+    const userId = req.auth.payload.sub;
 
-    // Find the user by their auth0Id
-    const user = await User.findOne({ auth0Id: userId })
+    const user = await User.findOne({ auth0Id: userId });
 
-    // Update the user's swipedIdeas array
     await User.updateOne(
       {
-        auth0Id: userId, // Use auth0Id instead of _id
+        auth0Id: userId,
         'swipedIdeas.idea': { $ne: ideaId },
       },
       {
@@ -95,73 +98,67 @@ export const swipe = async (req, res) => {
           },
         },
       }
-    )
+    );
 
-    // Implement the likes logic based on the action
     if (action === 'right') {
-      await Idea.findByIdAndUpdate(ideaId, { $inc: { likes: 1 } })
+      await Idea.findByIdAndUpdate(ideaId, { $inc: { likes: 1 } });
     } else if (action === 'left') {
-      await Idea.findByIdAndUpdate(ideaId, { $inc: { likes: -1 } })
+      await Idea.findByIdAndUpdate(ideaId, { $inc: { likes: -1 } });
     }
 
-    console.log('Swipe recorded successfully')
-    // Send a success response
-    res.status(200).json({ message: 'Swipe recorded successfully' })
+    console.log('Swipe recorded successfully');
+    res.status(200).json({ message: 'Swipe recorded successfully' });
   } catch (error) {
-    console.error(error)
+    console.error(error);
+    res
+    .status(500)
+    .json({ error: 'An error occurred while recording the swipe' });
+  }
+};
+
+export const claimIdea = async (req, res) => {
+  try {
+    console.log('DEBUG: Inside claimIdea');
+    const { ideaId } = req.body;
+    const userId = req.auth.payload.sub;
+
+    const user = await User.findOne({ auth0Id: userId });
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ status: 'fail', message: 'No user found with that ID' });
+    }
+
+    const idea = await Idea.findById(ideaId);
+
+    if (!idea) {
+      return res
+        .status(404)
+        .json({ status: 'fail', message: 'No idea found with that ID' });
+    }
+
+    // Update user credits and add the claimed idea to the user's claimedIdeas array
+    await User.updateOne(
+      { auth0Id: userId },
+      {
+        $inc: { credits: -idea.cost },
+        $addToSet: { claimedIdeas: ideaId },
+      }
+    );
+
+    // Update the idea's claimed status
+    await Idea.findByIdAndUpdate(ideaId, { $set: { isClaimed: true } });
+
+    // Fetch the updated user information
+    const updatedUser = await User.findOne({ auth0Id: userId });
+
+    console.log('Idea claimed successfully');
+    res.status(200).json({ message: 'Idea claimed successfully', data: updatedUser });
+  } catch (error) {
+    console.error(error);
     res
       .status(500)
-      .json({ error: 'An error occurred while recording the swipe' })
+      .json({ error: 'An error occurred while claiming the idea' });
   }
-}
-
-
-export const processClaim = async (req, res) => {
-
-try {
-  console.log('processing claim in controller...')
-  // Get the required information from the request body
-  const  ideaId  = req.body.ideaId
-  const idea = await Idea.findById(ideaId)
-  const userId = req.auth.payload.sub
-  const user = await User.findOne({ auth0Id: userId })
-  //const [balance, setBalance] = useState(user.credits || 0);
-  const balance = user["credits"];
-  const price = idea["price"];
-
-  console.log('idea id : ' + ideaId  + ' idea price: ' + price)
-  console.log('user id : ' + user.id + ' balance : ' + balance)
-
-  // delete idea from database
-  // const deletedIdea = await Idea.findByIdAndDelete(ideaId)
-  
-  // if (!deletedIdea) {
-  //   console.log('No idea found with id', ideaId);
-  // } else {
-  //   console.log('Idea with id', ideaId, 'deleted successfully');
-  // }
-
-  // TODO: add to claim idea array
-
-
-  if (balance >= price) {
-    console.log(`User has sufficient balance (${balance} credits)`);
-
-    const newBalance = balance - price;
-
-    user.credits = newBalance;
-    await user.save();
-    res.send(user);
-
-    console.log(`User current balance (${user.credits} credits)`);
-
-  } else {
-    console.log(`User has insufficient balance (${balance} credits)`);
-    alert("Insufficient credits!");
-    return;
-  }
-  
-} catch (error) {
-  res.status(500).json({ message: 'Error processing claim' })
-}
-}
+};
